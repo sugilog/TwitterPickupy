7e83589355103e210dfdc49b5a1a935c15835d72
sugilog <sugilog@gmail.com>
2017-01-14 18:19:56 +0900

    twitter api trial

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const Twitter = require( "twitter" ),
+      CONFIG  = require( "./config" ),
+      Tweet   = require( "./tweet" );
+
+const client = new Twitter( CONFIG );
+var params = { screen_name: "Rakuten_Panda" };
+
+client
+  .get( "statuses/user_timeline", params )
+  .then( ( tweets ) => {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    let index = Math.floor( Math.random() * tweets.length ),
+        tweet = tweets[ index ];
+
+    console.log( Tweet.userName( tweet ) );
+    console.log( Tweet.text( tweet ) );
+    console.log( Tweet.extra( tweet ) );
+  })
+  .catch( ( errors ) => {
+    if ( typeof errors.forEach === "function" ) {
+      errors.forEach( ( message ) => {
+        console.log( message );
+      });
+    }
+    else {
+      console.log( errors );
+    }
+  });
---
+++ tweet.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const userName = ( tweet ) => {
+  return `${ tweet.user.name } @${ tweet.user.screen_name }`;
+};
+
+const text = ( tweet ) => {
+  let text = tweet.text;
+
+  if ( hasMedia( tweet ) ) {
+    tweet.entities.media.forEach( ( media ) => {
+      text = text.replace( media.url, "" );
+    });
+  }
+
+  return text.trim();
+};
+
+const extra = ( tweet ) => {
+  return Object.assign(
+    {},
+    media( tweet ),
+    hashtags( tweet )
+  );
+};
+const media = ( tweet ) => {
+  let set = {};
+
+  if ( hasMedia( tweet ) ) {
+    tweet.entities.media.forEach( ( media ) => {
+      set[ media.type ] = set[ media.type ] || [];
+      set[ media.type ].push( media.media_url_https );
+    });
+  }
+
+  return set;
+};
+
+const hasMedia = ( tweet ) => {
+  return tweet.entities.media.length > 0;
+};
+
+const hashtags = ( tweet ) => {
+  if ( tweet.entities.hashtags.length > 0 ) {
+    return {
+      hashtags: tweet.entities.hashtags.map( ( hashtag ) => {
+        return `#${ hashtag.text }`;
+      })
+    };
+  }
+  else {
+    return {}
+  }
+};
+
+module.exports = {
+  userName: userName,
+  text:     text,
+  extra:    extra
+}
